1f4b8806fd472d321cf4f88d6c5c124de6a65b0f
Javier <jmartin@grupoloyal.com>
2024-10-21 14:01:48 +0200

    Proyecto

---
+++ Utilidades.xUnit.Tests/CalculadoraTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using Utilidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades.Tests {
+    public class CalculadoraTests {
+        [Fact()]
+        public void SumaTest() {
+            var sut = new Calculadora();
+
+            var actual = sut.Suma(1, 2);
+
+            Assert.Equal(3, actual);
+        }
+        [Fact()]
+        public void SumaKOTest() {
+            var sut = new Calculadora();
+
+            var actual = sut.Suma(1, -0.9);
+
+            Assert.Equal(0.3, actual);
+        }
+    }
+}
